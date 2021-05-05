baa50be49d029b2cd3c71f8c7abdbe364dd84013
Matheus <2002matheus@gmail.com>
2021-05-05 13:50:41 -0400

    Changes on txt game

--- TxtGame.c
+++ TxtGame.c
@@ -3,10 +3,11 @@
 
 void clearBuffer();
 void mapImpress(int position[2], int chestPos[2], int chestVerify);
-void playerCommand(char move[], int position[2], int *chestVerify);
+void playerCommand(char command[], int position[2], int *chestVerify);
 
 int main() {
-    char command[10] = "";
+    int cont = 0;
+    char command[10];
     int boole = 1;
     int posInit[] = {9, 4};
     int position[2] = {9, 4};
@@ -14,18 +15,20 @@ int main() {
     int chestVerify = 1;
     mapImpress(posInit, chestPos, chestVerify);
     while(boole != 0) {
-        printf("\nComando > ");
+        printf("\nCommand > ");
         scanf(" %9[^\n]", command);
         clearBuffer();
         if(strcmp(command, "exit") == 0) boole = 0;
-        if(strcmp(command, "reset")  == 0) {
-            for(int i = 0; i < 1; i++) position[i] = posInit[i];
-            chestVerify = 1;
-            mapImpress(position, chestPos, chestVerify);
-        }
-        else {
-            playerCommand(command, position, &chestVerify);
-            mapImpress(position, chestPos, chestVerify);
+        else{
+            if(strcmp(command, "reset")  == 0) {
+                for(int i = 0; i < 2; i++) position[i] = posInit[i];
+                chestVerify = 1;
+                mapImpress(position, chestPos, chestVerify);
+            }
+            else {
+                playerCommand(command, position, &chestVerify);
+                mapImpress(position, chestPos, chestVerify);
+            }
         }
     }
     return 0;
@@ -52,36 +55,39 @@ void mapImpress(int position[2], int chestPos[2], int chestVerify) {
     }
 }
 
-void playerCommand(char move[], int position[2], int *chestVerify) {
-    if(strcmp(move, "w") == 0) {
+void playerCommand(char command[], int position[2], int *chestVerify) {
+    if(strcmp(command, "w") == 0) {
         if(position[1] == 0) position[1] = 9;
         else {
             if(position[0] == 19 && position[1] == 7);
             else position[1]--;
         }
     }
-    if(strcmp(move, "s") == 0) {
+    if(strcmp(command, "s") == 0) {
         if(position[1] == 9) position[1] = 0;
         else{
             if(position[0] == 19 && position[1] == 5);
             else position[1]++;
         }
     }
-    if(strcmp(move, "a") == 0) {
+    if(strcmp(command, "a") == 0) {
         if(position[0] == 0 && position[1] == 6);
         else {
             if(position[0] == 0) position[0] = 19;
             else position[0]--;
         }
     }
-    if(strcmp(move, "d") == 0) {
+    if(strcmp(command, "d") == 0) {
         if(position[0] == 19) position[0] = 0;
         else {
             if(position[0] == 18 && position[1] == 6);
             else position[0]++;
         }
     }
-    if(((position[0] == 18 && position[1] == 6) || (position[0] == 19 && position[1] == 5) || (position[0] == 19 && position[1] == 7)) && strcmp(move, "open") == 0) {
+    if(((position[0] == 18 && position[1] == 6) || (position[0] == 19 && position[1] == 5) || (position[0] == 19 && position[1] == 7)) && strcmp(command, "open") == 0) {
         *chestVerify = 0;
+    }
+    if(((position[0] == 18 && position[1] == 6) || (position[0] == 19 && position[1] == 5) || (position[0] == 19 && position[1] == 7)) && strcmp(command, "close") == 0) {
+        *chestVerify = 1;
     }
 }
